671c38bd345df098c2ad16b0a4eb4c4d0d6f47b8
ferchuqui <fernando.farji@gmail.com>
2021-06-27 11:02:23 -0300

    Add and delete items

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'store';
+  title = 'Articulos';
+  items = ['nicolas', 'julian', 'perez'];
+  addItem(){
+    this.items.push('nuevo item');
+  }
+  deleteItem(index: number){
+    this.items.splice(index, 1)
+  }
 }
+
