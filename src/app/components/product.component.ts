015a247e71c09323c84bc70817ec2f948a75e122
ferchuqui <fernando.farji@gmail.com>
2021-07-05 00:00:24 -0300

    Ciclo de Vida

--- src/app/components/product.component.ts
+++ src/app/components/product.component.ts
@@ -1,19 +1,34 @@
-import { Component, Input, Output,EventEmitter, OnInit } from '@angular/core';
+import { Component, Input, Output,EventEmitter, OnChanges,  OnInit, SimpleChanges, DoCheck, OnDestroy } from '@angular/core';
 import { Product } from '../product.model';
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.scss']
 })
-export class ProductComponent implements OnInit {
+export class ProductComponent implements OnDestroy, OnInit, DoCheck {
 
   @Input() product: Product;
   @Output() productClicked: EventEmitter<any> = new EventEmitter();
-  addCart(){
+  constructor() {
+    console.log('1. constructor');
+  }
+    // ngOnChanges(changes: SimpleChanges) {
+    //   console.log('2. ngOnChanges');
+    //   console.log(changes);
+    // }
+  addCart() {
     console.log('añadir al carrito');
     this.productClicked.emit(this.product.id);
   }
   ngOnInit() {
+    console.log('3. ngOnInit');
   }
+
+  ngDoCheck() {
+    console.log('4. doCheck');
+  };
+  ngOnDestroy() {
+    console.log('5. ngOnDestroy');
+  };
 
 }
