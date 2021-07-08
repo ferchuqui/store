021cb8ffd314c324b0ef24ff568404330e077a06
ferchuqui <fernando.farji@gmail.com>
2021-07-07 23:28:21 -0300

    Pipes

--- src/app/components/product.component.ts
+++ src/app/components/product.component.ts
@@ -9,6 +9,7 @@ export class ProductComponent implements OnDestroy, OnInit, DoCheck {
 
   @Input() product: Product;
   @Output() productClicked: EventEmitter<any> = new EventEmitter();
+  today = new Date();
   constructor() {
     console.log('1. constructor');
   }
